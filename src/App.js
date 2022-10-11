9400723bef3e2d7933b48c1604649257cfb4edaf
KD3821 <80853241+KD3821@users.noreply.github.com>
2022-10-11 23:51:40 +0300

    some edits done

--- src/App.js
+++ src/App.js
@@ -1,102 +1,71 @@
 import React, {useRef, useState} from 'react';
 import './styles/App.css';
-import RideItem from "./components/RideItem";
 import RideList from "./components/RideList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [rides, setRides] = useState([
-        {
-            id: 1,
-            number: '0001',
-            date: '2022-08-07',
-            price: 1090,
-            car: 'M129KH198',
-            driver: 'Дементьев'
-        },
-        {
-            id: 2,
-            number: '0002',
-            date: '2022-08-07',
-            price: 1500,
-            car: 'E212HB198',
-            driver: 'Павлюченков'
-        },
-        {
-            id: 3,
-            number: '0003',
-            date: '2022-08-07',
-            price: 890,
-            car: 'E975MK198',
-            driver: 'Шевнин'
-        },
+        { id: 1, number: '0001', date: '2022-08-07', price: 1090, car: 'M129KH198', driver: 'Дементьев'},
+        { id: 2, number: '0002', date: '2022-08-07', price: 1500, car: 'E212HB198', driver: 'Павлюченков'},
+        { id: 3, number: '0003', date: '2022-08-07', price: 890, car: 'E975MK198', driver: 'Шевнин'},
     ])
 
-    const [date, setDate] = useState('');
-    const [driver, setDriver] = useState('');
-    const [car, setCar] = useState('');
-    const [number, setNumber] = useState('');
-    const [price, setPrice] = useState('');
-
-
+    const [ride, setRide] = useState({
+        number: '',
+        date: '',
+        price: '',
+        car: '',
+        driver: ''
+    })
 
     const addNewRide = (e) => {
         e.preventDefault()
-        const newRide = {
-            id: Date.now(),
-            date,
-            driver,
-            car,
-            number,
-            price
-        }
-        console.log(newRide)
-        setRides([...rides, newRide])
-        setDate('')
-        setDriver('')
-        setCar('')
-        setNumber('')
-        setPrice('')
-
+        setRides([...rides, {...ride, id: Date.now()}])
+        setRide({
+            number: '',
+            date: '',
+            price: '',
+            car: '',
+            driver: ''
+        })
     }
-
     return (
     <div className="App">
         <form>
             <MyInput
-                value={date}
-                onChange={e => setDate(e.target.value)}
+                value={ride.date}
+                onChange={e => setRide({...ride, date: e.target.value})}
                 type="text"
                 placeholder="Дата"
             />
             <MyInput
-                value={driver}
-                onChange={e => setDriver(e.target.value)}
+                value={ride.driver}
+                onChange={e => setRide({...ride, driver: e.target.value})}
                 type="text"
                 placeholder="Водитель"
             />
             <MyInput
-                value={car}
-                onChange={e => setCar(e.target.value)}
+                value={ride.car}
+                onChange={e => setRide({...ride, car: e.target.value})}
                 type="text"
                 placeholder="Авто"
             />
             <MyInput
-                value={number}
-                onChange={e => setNumber(e.target.value)}
+                value={ride.number}
+                onChange={e => setRide({...ride, number: e.target.value})}
                 type="text"
                 placeholder="Номер"
             />
             <MyInput
-                value={price}
-                onChange={e => setPrice(e.target.value)}
+                value={ride.price}
+                onChange={e => setRide({...ride, price: e.target.value})}
                 type="text"
                 placeholder="Cтоимость"
             />
             <MyButton onClick={addNewRide}>Добавить поездку</MyButton>
         </form>
-        <RideList orders={rides} title="Список постов 1"/>
+        <RideList orders={rides} title="Список постов"/>
     </div>
     );
 }
